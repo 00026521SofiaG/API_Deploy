9abb6a340cd4f1fc8ff64278ede8c9fa00c7b545
SofiaG <90490408+00026521SofiaG@users.noreply.github.com>
2023-06-29 19:47:59 -0600

    Changed PORT

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ import  express  from 'express'
 
 const app = express()
 
-app.listen(3000, ()=> console.log('API running on port 3000'))
+app.listen(3001, ()=> console.log('API running on port 3000'))
 
 app.get('/', (req,res)=> res.json('This is a deployed API'))
