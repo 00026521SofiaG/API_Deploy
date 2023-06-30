916b4da112be41a9a554d0617cb02135bc62d0ce
SofiaG <90490408+00026521SofiaG@users.noreply.github.com>
2023-06-30 14:33:49 -0600

    Update index.js
    ahhhhhhh

--- index.js
+++ index.js
@@ -5,4 +5,4 @@ const app = express()
 
 app.listen(3001, ()=> console.log('API running on port 3001'))
 
-app.get('/', (req,res)=> res.json('This is a deployed API'))
+app.get('/api/texto', (req,res)=> res.json('This is a deployed API'))
